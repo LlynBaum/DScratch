0edc25a57d43c37e150599fa18bef499037c9c7e
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2026-03-22 20:26:48 +0100

    add more commands to ui

--- DScratch.Client/Components/EditorMenu.razor.cs
+++ DScratch.Client/Components/EditorMenu.razor.cs
@@ -8,4 +8,29 @@ public partial class EditorMenu(UpdateDispatcher updateDispatcher)
     {
         await updateDispatcher.DispatchAsync(Commands.ToggleMark.Strong);
     }
+
+    private async Task ItalicAsync()
+    {
+        await updateDispatcher.DispatchAsync(Commands.ToggleMark.Italic);
+    }
+    
+    private async Task CodeAsync()
+    {
+        await updateDispatcher.DispatchAsync(Commands.ToggleMark.Code);
+    }
+
+    private async Task ParagraphAsync()
+    {
+        await updateDispatcher.DispatchAsync(Commands.SetBlockType.Paragraph);
+    }
+    
+    private async Task HeadingAsync(ushort level)
+    {
+        await updateDispatcher.DispatchAsync(Commands.SetBlockType.Heading(level));
+    }
+    
+    private async Task CodeBlockAsync()
+    {
+        await updateDispatcher.DispatchAsync(Commands.SetBlockType.CodeBlock);
+    }
 }
