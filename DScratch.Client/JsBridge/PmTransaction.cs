8e05f161baf01c9be90801c2ba9d130aa728523b
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2026-04-22 21:34:01 +0200

    add more steps to transaction

--- DScratch.Client/JsBridge/PmTransaction.cs
+++ DScratch.Client/JsBridge/PmTransaction.cs
@@ -30,37 +30,133 @@ public class PmTransaction(IJSRuntime jsRuntime)
         await jsRuntime.InvokeVoidAsync(TransactionJsMethod, steps);
     }
 
-    public PmTransaction Replace() // TODO: slice param
+    /// <summary>
+    /// Replace current selection with given Slice.
+    /// </summary>
+    /// <returns>The Transaction for chaining.</returns>
+    public PmTransaction Replace(PmSlice? slice = null)
     {
-        AddStep("replace");
+        AddStep("replace", new Dictionary<string, object?>
+        {
+            { "slice", slice }
+        });
         return this;
     }
     
-    public PmTransaction Replace(int from, int? to = null) // TODO: slice param
+    /// <summary>
+    /// Replace the part of the document between from and to with the given slice.
+    /// </summary>
+    /// <returns>The Transaction for chaining.</returns>
+    public PmTransaction Replace(int from, int? to = null, PmSlice? slice = null)
     {
         AddStep("replace", new Dictionary<string, object?>
         {
             { "from", from },
-            { "to", to }
+            { "to", to },
+            { "slice", slice }
         });
 
         return this;
     }
 
-    public PmTransaction ReplaceWith(PmNode node)
+    /// <summary>
+    /// Replace the current selection with the given content, which may be a node or array of nodes.
+    /// </summary>
+    /// <returns>The Transaction for chaining.</returns>
+    public PmTransaction ReplaceWith(params PmNode[] node)
     {
         AddStep("replaceWith", new Dictionary<string, object?>
         {
-            { "node", node }
+            { "nodes", node }
         });
         return this;
     }
     
+    /// <summary>
+    /// Replace the given range with the given content, which may be a node or array of nodes.
+    /// </summary>
+    /// <returns>The Transaction for chaining.</returns>
     public PmTransaction ReplaceWith(PmNode node, int from, int? to = null)
     {
         AddStep("replaceWith", new Dictionary<string, object?>
         {
+            { "nodes", node },
+            { "from", from },
+            { "to", to }
+        });
+        return this;
+    }
+
+    /// <summary>
+    /// Delete the content between the given positions.
+    /// </summary>
+    /// <returns>The Transaction for chaining.</returns>
+    public PmTransaction Delete(int from, int to)
+    {
+        AddStep("delete", new Dictionary<string, object?>
+        {
+            { "from", from },
+            { "to", to }
+        });
+        return this;
+    }
+    
+    /// <summary>
+    /// Insert the given content at the given position.
+    /// </summary>
+    /// <returns>The Transaction for chaining.</returns>
+    public PmTransaction Insert(int position, params PmNode[] nodes)
+    {
+        AddStep("insert", new Dictionary<string, object?>
+        {
+            { "pos", position },
+            { "content", nodes }
+        });
+        return this;
+    }
+
+    /// <summary>
+    /// Replace a range of the document with a given slice, using from nad to.
+    /// The similar replace method is a more primitive tool which will not move the start and end of its given range,
+    /// and is useful in situations where you need more precise control over what happens.
+    /// </summary>
+    /// <returns>The Transaction for chaining.</returns>
+    public PmTransaction ReplaceRange(int from, int to, PmSlice slice)
+    {
+        AddStep("replaceRange", new Dictionary<string, object?>
+        {
+            { "from", from },
+            { "to", to },
+            { "slice", slice }
+        });
+        return this;
+    }
+    
+    /// <summary>
+    /// Replace the given range with a node, but use from and to as hints, rather than precise positions.
+    /// When from and to are the same and are at the start or end of a parent node in which the given node doesn't fit,
+    /// this method may move them out towards a parent that does allow the given node to be placed.
+    /// </summary>
+    /// <returns>The Transaction for chaining.</returns>
+    public PmTransaction ReplaceRangeWith(PmNode node, int from, int to)
+    {
+        AddStep("replaceRangeWith", new Dictionary<string, object?>
+        {
             { "node", node },
+            { "from", from },
+            { "to", to }
+        });
+        return this;
+    }
+    
+    /// <summary>
+    /// Delete the given range, expanding it to cover fully covered parent nodes until a valid replacement is found
+    /// </summary>
+    /// <returns>The Transaction for chaining.</returns>
+    public PmTransaction DeleteRange(int from, int to)
+    {
+        AddStep("deleteRange", new Dictionary<string, object?>
+        {
             { "from", from },
             { "to", to }
         });
--- DScratch.Client/Scripts/cSharpTransaction.ts
+++ DScratch.Client/Scripts/cSharpTransaction.ts
@@ -12,11 +12,36 @@ export const dispatchCSharp = (state: EditorState, dispatch: (tr: Transaction) =
     steps.forEach(step => {
         switch (step.name) {
             case "replace":
-                tr = tr.replace(step.args.from ?? state.selection.from, step.args.to);
+                const slice = sliceFrom(step.args.slice);
+                
+                tr = tr.replace(step.args.from ?? state.selection.from, step.args.to, slice);
                 break;
             case "replaceWith": {
-                const nodes: readonly Node[] = step.args.nodes.map((n: any) => state.schema.nodes[n.name].create(n.attrs))
+                const nodes = createNodes(step.args.nodes);
                 tr = tr.replaceWith(step.args.from, step.args.to, nodes);
+                break;
+            }
+            case "delete": {
+                tr = tr.delete(step.args.from, step.args.to);
+                break;
+            }
+            case "insert": {
+                const content = createNodes(step.args.content);
+                tr = tr.insert(step.args.pos, content);
+                break;
+            }
+            case "replaceRange": {
+                const slice = sliceFrom(step.args.slice);
+                tr = tr.replaceRange(step.args.from, step.args.to, slice!);
+                break;
+            }
+            case "replaceRangeWith": {
+                const node = createNode(step.args.node);
+                tr = tr.replaceRangeWith(step.args.from, step.args.to, node);
+                break;
+            }
+            case "deleteRange": {
+                tr = tr.deleteRange(step.args.from, step.args.to);
                 break;
             }
         }
@@ -24,5 +49,26 @@ export const dispatchCSharp = (state: EditorState, dispatch: (tr: Transaction) =
 
     if (tr.docChanged || tr.selectionSet) {
         dispatch(tr);
+    }
+
+    return;
+
+    function createNode(node: any) : Node {
+        return state.schema.nodes[node.name].create(node.attrs)
+    }
+    
+    function createNodes(nodes: any[]) : Node[] {
+        return nodes.map(createNode)
+    }
+    
+    function sliceFrom(args: any) {
+        if(!args){
+            return undefined;
+        }
+        
+        return state.doc.slice(
+            args.slice.Start,
+            args.slice.End,
+            args.slice.IncludeParents);
     }
 }
