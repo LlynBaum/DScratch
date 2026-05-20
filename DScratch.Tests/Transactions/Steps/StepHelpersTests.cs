400d8ab5bb5d165a0a7bc6b9762a2f4e914e169d
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2026-05-20 21:33:20 +0200

    start testing toinsert node

--- DScratch.Tests/Transactions/Steps/StepHelpersTests.cs
+++ DScratch.Tests/Transactions/Steps/StepHelpersTests.cs
@@ -1,6 +1,51 @@
+using DScratch.Nodes;
+using DScratch.Transactions;
+using DScratch.Transactions.Steps;
+
 namespace DScratch.Tests.Transactions.Steps;
 
 public class StepHelpersTests
 {
-    // TODO
+    private class ToInsert
+    {
+        [Test]
+        public void CharNode_ReturnsInsertTextDiff()
+        {
+            var node = new CharNode('a', "2", null, null, null);
+            var path = new NodePath(["1"]);
+
+            var result = node.ToInsert(path, 42);
+            
+            Assert.That(result, Has.Length.EqualTo(1));
+            Assert.That(result.Single(), Is.TypeOf<StepDiff.InsertTextDiff>());
+            
+            var step = (StepDiff.InsertTextDiff)result.Single();
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(step.Parent, Is.EquivalentTo(["1"]));
+                Assert.That(step.Offset, Is.EqualTo(42));
+                Assert.That(step.Text, Is.EqualTo("a"));
+            }
+        }
+        
+        [Test]
+        public void TextElement_ReturnsExpectedSteps()
+        {
+        }
+        
+        [Test]
+        public void Element_ReturnsInsertElementDiff()
+        {
+        }
+        
+        [Test]
+        public void UnknownNodes_ThrowArgumentException()
+        {
+        }
+    }
+    
+    private class ToDelete
+    {
+        
+    }
 }
