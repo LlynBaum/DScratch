9dfb6f7e115dd0a1dd556a2f0b4c628f2997246d
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2026-05-20 12:55:19 +0200

    some more js transaction stuff

--- DScratch.Client/Scripts/transaction.ts
+++ DScratch.Client/Scripts/transaction.ts
@@ -1,69 +1,78 @@
 enum StepType {
     insertText = "insertText",
     deleteText = "deleteText",
+    splitText = "splitText", // TODO: maybe useful as well?
     insertElement = "insertElement",
     deleteElement = "deleteElement",
-    move = "move"
+    move = "move",
 }
 
-interface StepDiff {
+interface Step {
     Type: StepType;
 }
 
-interface InsertTextDiff extends StepDiff {
+interface InsertTextStep extends Step {
     Parent: string[];
     Offset: number;
     Text: string;
 }
 
-interface DeleteTextDiff extends StepDiff {
+interface DeleteTextStep extends Step {
     Parent: string[]; 
     Offset: number; 
     Length: number;
 }
 
-interface InsertElementDiff extends StepDiff {
+interface SplitTextStep extends Step {
+    TargetNodePath: string[];
+    Offset: number;
+}
+
+interface InsertElementStep extends Step {
     Parent: string[];
     Offset: number;
     TagName: string;
     NewNodeId: string;
 }
 
-interface DeleteElementDiff extends StepDiff {
+interface DeleteElementStep extends Step {
     Path: string[];
 }
 
-interface MoveDiff extends StepDiff {
+interface MoveStep extends Step {
     TargetNodePath: string[];
     TargetParentPath: string[];
     TargetOffset: number;
 }
 
-export function applyTransaction(transaction: StepDiff[]){
+export function applyTransaction(transaction: Step[]){
     transaction.map(handle);
     
-    function handle(step: StepDiff) {
+    function handle(step: Step) {
         switch (step.Type) {
             case StepType.insertText:
-                handleInsertTextDiff(step as InsertTextDiff);
+                handleInsertTextStep(step as InsertTextStep);
                 break;
             case StepType.deleteText:
-                handleDeleteTextDiff(step as DeleteTextDiff);
+                handleDeleteTextStep(step as DeleteTextStep);
+                break;
+            case StepType.splitText:
+                handleSplitTextStep(step as SplitTextStep);
                 break;
             case StepType.insertElement:
-                handleInsertElementDiff(step as InsertElementDiff);
+                handleInsertElementStep(step as InsertElementStep);
                 break;
             case StepType.deleteElement:
-                handleDeleteElementDiff(step as DeleteElementDiff);
+                handleDeleteElementStep(step as DeleteElementStep);
                 break;
             case StepType.move:
-                handleMoveDiff(step as MoveDiff);
+                handleMoveStep(step as MoveStep);
                 break;
         }
     }
 }
 
-function handleInsertTextDiff(step: InsertTextDiff) {
+function handleInsertTextStep(step: InsertTextStep) {
     const element = findNode(step.Parent);
     if (!element) return;
 
@@ -72,7 +81,7 @@ function handleInsertTextDiff(step: InsertTextDiff) {
     element.innerText = text.slice(0, step.Offset) + step.Text + text.slice(step.Offset);
 }
 
-function handleDeleteTextDiff(step: DeleteTextDiff) {
+function handleDeleteTextStep(step: DeleteTextStep) {
     const element = findNode(step.Parent);
     if (!element) return;
 
@@ -81,7 +90,18 @@ function handleDeleteTextDiff(step: DeleteTextDiff) {
     element.innerText = text.slice(0, step.Offset) + text.slice(step.Offset + step.Length);
 }
 
-function handleInsertElementDiff(step: InsertElementDiff) {
+function handleSplitTextStep(step: SplitTextStep) {
+    const element = findNode(step.TargetNodePath);
+    if (!element) return;
+
+    // TODO: split only allows to split text, everything else can be done with move. But does it do that xD
+    // TODO: maybe do it fancy like insertElement...?
+    if(element instanceof Text) {
+        element.splitText(step.Offset);
+    }
+}
+
+function handleInsertElementStep(step: InsertElementStep) {
     const parent = findNode(step.Parent);
     if (!parent) return;
     
@@ -90,14 +110,14 @@ function handleInsertElementDiff(step: InsertElementDiff) {
     insertElement(element, parent, step.Offset);
 }
 
-function handleDeleteElementDiff(step: DeleteElementDiff) {
+function handleDeleteElementStep(step: DeleteElementStep) {
     const element = findNode(step.Path);
     if (!element) return;
     
     element.remove();
 }
 
-function handleMoveDiff(step: MoveDiff) {
+function handleMoveStep(step: MoveStep) {
     const element = findNode(step.TargetNodePath);
     if (!element) return;
     
@@ -108,6 +128,7 @@ function handleMoveDiff(step: MoveDiff) {
     insertElement(element, newParent, step.TargetOffset);
 }
 
+// TODO: really that fancy. I mean it HAS to be the given parent. Everything else might break my C# stuff
 function insertElement(element: Element, parent: Element, offset: number) {
     if(!parent.hasChildNodes()){
         parent.appendChild(element);
