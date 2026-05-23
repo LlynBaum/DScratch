57f7c70b3fd0401ad4d4dad14247da1c317c44b4
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2026-05-23 21:11:27 +0200

    little reminder

--- DScratch/Transactions/Steps/DeleteStep.cs
+++ DScratch/Transactions/Steps/DeleteStep.cs
@@ -6,6 +6,7 @@ public class DeleteStep(DNode node) : IStep
 {
     public IReadOnlyList<StepDiff> Execute(DScratchDocument document)
     {
+        // must be called before .Delete(), else the node can not be found anymore
         var step = node.ToDeleteSteps();
         node.Delete();
         return [step];
