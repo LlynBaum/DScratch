616d5613f2297b95949b8fe54b45bd53c6e61594
Llyn Baumann <llyn.baumann@digitecgalaxus.ch>
2026-07-05 18:11:37 +0200

    render page as HTML

---
+++ src/DScratch.Client/BrowserInteractions/Rendering/DPage.razor.cs
@@ -0,0 +1,10 @@
+﻿using DScratch.LayoutEngine;
+using Microsoft.AspNetCore.Components;
+
+namespace DScratch.Client.BrowserInteractions.Rendering;
+
+public partial class DPage : ComponentBase
+{
+    [Parameter]
+    public ElementNode RootElement { get; set; }
+}
---
+++ src/DScratch.Client/BrowserInteractions/Rendering/LayoutRenderer.cs
@@ -0,0 +1,25 @@
+﻿using DScratch.Interactions;
+using DScratch.LayoutEngine;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace DScratch.Client.BrowserInteractions.Rendering;
+
+public class LayoutRenderer(IServiceProvider serviceProvider, ILoggerFactory loggerFactory) : ILayoutRenderer
+{
+    public async Task RenderAsync(ElementNode rootElement, SelectionInfo? cursorPosition)
+    {
+        await using var htmlRenderer = new HtmlRenderer(serviceProvider, loggerFactory);
+        
+        var parameterDictionary = new Dictionary<string, object?>
+        {
+            { nameof(DPage.RootElement), rootElement }
+        };
+        
+        var parameters = ParameterView.FromDictionary(parameterDictionary);
+        var output = await htmlRenderer.RenderComponentAsync<DPage>(parameters);
+        var htmlString = output.ToHtmlString();
+        
+        // TODO: send via JsInvoker to JS and swap DOM
+    }
+}
